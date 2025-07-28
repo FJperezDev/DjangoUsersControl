a505c4b1b3f8622546c44b81a1ed90901dc3fbf3
Fran1763 <franciscojavier.1763@gmail.com>
2025-07-28 15:58:37 +0200

    Login working, added route to /

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import UserViewSet, RegisterView
+from .views import UserViewSet, RegisterView, LoginView, LogoutView, LoggedUserView
 
-from django.contrib.auth.views import LoginView, LogoutView
+# from django.contrib.auth.views import LoginView, LogoutView
 
 router = routers.DefaultRouter()
 
@@ -15,6 +15,7 @@ urlpatterns += [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('account/profile', LoggedUserView.as_view(), name='profile'),
 ]
 
 # This will automatically create the URL patterns for the ProjectViewSet, allowing CRUD operations on the Project model.
--- api/views/__init__.py
+++ api/views/__init__.py
@@ -2,3 +2,4 @@ from .userViewSet import UserViewSet
 from .loginView import LoginView
 from .registerView import RegisterView
 from .logoutView import LogoutView
+from .loggedUserView import LoggedUserView
